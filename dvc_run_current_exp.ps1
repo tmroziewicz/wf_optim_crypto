507e651f630e7e752ab778389e0cf544a5dc9d38
mrozo <mrozoo@o2.pl>
2026-01-04 21:01:20 +0100

    Before running global train

---
+++ dvc_run_current_exp.ps1
@@ -0,0 +1,15 @@
+﻿$names = dvc queue status  | ForEach-Object { 
+    $line = $_.Trim()
+    if ($line -like "*Queued*") {
+             ($line -split '\s+')[1] } 
+}
+      
+Write-Host $names
+
+
+foreach ($n in $names) {
+    if ($n) {
+        Write-Host "Processing Experiment: $n" -ForegroundColor Green
+        dvc exp run --name $n
+    }
+}
