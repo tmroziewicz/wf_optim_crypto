a6b5a342f255a49315c2447af4fb40a1aa65c7aa
mrozo <mrozoo@o2.pl>
2026-01-04 19:35:18 +0100

    git cache remove rds

---
+++ remove_cache_git.bat
@@ -0,0 +1,8 @@
+git rm -r --cached  master\data-wip\0\60\02_downsampled.rds
+git rm -r --cached  master\data-wip\0\60\03_precalculated.rds
+git rm -r --cached  master\data-wip\0\60\04_positions.rds
+git rm -r --cached  master\data-wip\0\60\05_pnl.rds
+git rm -r --cached  master\data-wip\0\60\06_wf_indexes.rds
+git rm -r --cached  master\data-wip\0\60\07_wf_metrics.rds
+git rm -r --cached  master\data-wip\0\60\08_wf_best_params.rds
+git rm -r --cached  master\data-wip\0\60\09_wf_test_evalued.rds
