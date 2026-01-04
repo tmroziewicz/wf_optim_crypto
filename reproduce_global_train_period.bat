133c723347f8b1e6bb0fb52be3791b05229d3481
mrozo <mrozoo@o2.pl>
2026-01-04 21:18:57 +0100

    Global train added

--- reproduce_global_train_period.bat
+++ reproduce_global_train_period.bat
@@ -1,1 +1,1 @@
-dvc exp run --queue -S  general.tfmin=1,5,10,15,30,60  -S wf.train_length=1,2,3,5,7,10,14,21,28  -S wf.test_length=1,2,3,5,7,10,14,21,28  -S general.performance_stat=sharpe
+dvc exp run --queue -S  general.tfmin=1,5,10,15,30,60  -S wf.train_length=1,2,3,5,7,10,14,21,28  -S wf.test_length=1,2,3,5,7,10,14,21,28  -S general.performance_stat=sharpe -S general.raw_data=master/data-raw/data_global_train_20180101_20190930.csv
